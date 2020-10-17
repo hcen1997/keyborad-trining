789be8029b81f3d9a45281a2af0e6b0abe86cb16
hcen1997 <panzetaoyx@gmail.com>
2020-10-18 01:12:55 +0800

    mvp version

--- main.py
+++ main.py
@@ -5,41 +5,65 @@
 # typing
 
 # and get a fuking log in /var/log/${repName}/yyyy-mm-dd.log
-# echoe
+# echo every line_info to log
 import secrets
+import os
 
 keySet = [1, 2, 3, 4, 5, 6, 7, 8, 9, 0]
 
-first = True
+total = 0
+right = 0
+current_target = 0
 
 
-def printFirst():
+def First():
+    clear()
     print("current_set=", keySet)
+    print("press digit to start, alpha to exit. ", end="")
+    input()
 
 
 def printPractisInfo():
-    line = {
-        "total":secrets.choice(keySet),
-        "score":secrets.choice(keySet),
-        "try_type":secrets.choice(keySet),
-    }
-
-    print(line)
+    global current_target
+    current_target = secrets.choice(keySet)
+    print({
+        "try_type": current_target,
+        "total": total,
+        "score": 0.00 if total == 0 else (right / total) * 100,
+    })
 
 
 def info_head():
-    global first
-    if not first:
-        printPractisInfo()
-    else:
-        first = False
-        printFirst()
+    printPractisInfo()
+
+def handleInput():
+    c: str = input()
+    if c.isalpha():
+        exit()
+    return int(c)
 
 
 def type_body():
-    pass
+    global right
+    global total
+    print("> ", end="")
+    c = handleInput()
+    total += 1
+    if c == current_target:
+        right += 1
 
 
-while True:
-    info_head()
-    type_body()
+def clear():
+    os.system('clear')
+
+
+def main():
+    First()
+    while True:
+        info_head()
+        type_body()
+        clear()
+
+
+if __name__ == '__main__':
+    main()
