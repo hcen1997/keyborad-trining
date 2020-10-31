ade64c4cafa62c8c2a8d86eb92a4551eee900486
hcen1997 <panzetaoyx@gmail.com>
2020-10-31 14:02:07 +0800

    release: can determine where you are a sucker

--- src/main/java/KeywordTrainer.java
+++ src/main/java/KeywordTrainer.java
@@ -1,10 +1,14 @@
+import lombok.SneakyThrows;
 import pojo.TrainContext;
 
+import java.io.IOException;
 import java.util.Arrays;
 import java.util.concurrent.ThreadLocalRandom;
 
 public class KeywordTrainer {
     public static final TrainContext trainContext = new TrainContext();
+    private static final byte[] inputBuffer = new byte[10];
+
     public static void main(String[] args) {
         first();
         for (int i = 0; i < TrainContext.defaultTrainTime; i++) {
@@ -17,13 +21,18 @@ public class KeywordTrainer {
         System.out.println("press digit to start, alpha to exit. ");
     }
 
+    @SneakyThrows
     private static void train() {
-        info_head()
-        t = Process(target = check)
-        t.start()
-        type_body()
-        clear(t)
-        print(current_typing)
+        info_head();
+//        timer start
+//        type_body()
+        // why show '10'? because sout('\n')=='10'
+        System.in.read(inputBuffer);
+        if(inputBuffer[0]-'0' == trainContext.getTryType()){
+            System.out.println("you are a winner");
+        }else {
+            System.out.println("sucker~");
+        }
     }
 
     private static void info_head() {
