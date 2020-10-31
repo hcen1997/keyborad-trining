f5e4cb7bbcf5dc93c0c92645f611431863fafa10
hcen1997 <panzetaoyx@gmail.com>
2020-10-31 11:41:44 +0800

    organize folder

---
+++ src/main/java/KeywordTrainer.java
@@ -0,0 +1,15 @@
+public class KeywordTrainer {
+    public static final int[] keySet = {1, 2, 3, 4, 5, 6, 7, 8, 9, 0};
+    public static final double timeLimit = 1.5;
+
+    public static void main(String[] args) {
+        first();
+    }
+
+    private static void first() {
+        System.out.println();("current_set=", keySet)
+        System.out.println("press digit to start, alpha to exit. ", end="")
+    }
+
+
+}
